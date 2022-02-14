e27e5e59a6fed99e44c1722352250585e24cc978
Nirmeet <nirmeet.baweja@gmail.com>
2022-02-14 00:15:53 +0000

    user input

--- user-input.cpp
+++ user-input.cpp
@@ -1,3 +1,28 @@
+/**
+ * @file user-input.cpp
+ * @author nirmeet baweja
+ * @brief User interface contains two types of user input controls: TextInput,
+ * which accepts all characters and NumericInput, which accepts only digits
+ *
+ * Implement the following methods:
+ * add on class TextInput - adds the given character to the current value
+ * getValue on class TextInput - returns the current value add on class
+ * NumericInput - overrides the base class method so that each non-numeric
+ * character is ignored
+ *
+ * For example, the following code should output "10":
+ * TextInput* input = new NumericInput();
+ * input->add('1');
+ * input->add('a');
+ * input->add('0');
+ * std::cout << input->getValue();
+ *
+ * @version 0.1
+ * @date 2022-02-13
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <iostream>
 #include <string>
 
@@ -9,19 +34,16 @@ private:
 public:
   TextInput()
   {
-    // std::cout << "text const\n";
     input_variable = "";
   }
 
   virtual void add(char c)
   {
-    // std::cout << "text add\n";
     this->input_variable.push_back(c);
   }
 
   std::string getValue()
   {
-    // std::cout << "input variable : " << input_variable << '\n';
     return input_variable;
   }
 };
@@ -34,10 +56,8 @@ public:
 
 void NumericInput::add(char c)
 {
-  // std::cout << "num add\n";
   if (c >= '0' && c <= '9')
   {
-    // std::cout << "c: " << c << '\n';
     TextInput::add(c);
   }
 }
@@ -45,7 +65,6 @@ void NumericInput::add(char c)
 #ifndef RunTests
 int main()
 {
-  // std::cout << "main\n";
   TextInput *input = new NumericInput();
   input->add('1');
   input->add('a');
