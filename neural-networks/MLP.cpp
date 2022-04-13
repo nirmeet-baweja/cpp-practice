7f25d1fd5d4d6ebd0c74327ffc4428b3ed536099
Nirmeet <nirmeet.baweja@gmail.com>
2022-04-13 12:21:54 +0100

    added function for backpropagation

--- neural-networks/MLP.cpp
+++ neural-networks/MLP.cpp
@@ -60,11 +60,14 @@ MultiLayerPerceptron::MultiLayerPerceptron(vector<int> layers, double bias, doub
   for (unsigned long int i = 0; i < layers.size(); i++)
   {
     values.push_back(vector<double>(layers[i], 0.0));
+    d.push_back(vector<double>(layers[i], 0.0));
     network.push_back(vector<Perceptron>());
     if (i > 0)
     {
       for (int j = 0; j < layers[i]; j++)
       {
+        // for each value of j add a new perceptron to the network[i] layer
+        // the value of previous layer becomes the number of inputs for current layer
         network[i].push_back(Perceptron(layers[i - 1], bias));
       }
     }
@@ -126,4 +129,88 @@ vector<double> MultiLayerPerceptron::run(vector<double> x)
     }
   }
   return values.back();
+}
+
+/**
+ * @brief function to calculate mean squared error
+ *
+ */
+double mean_squared_error(vector<double> predicted_value, vector<double> actual_value)
+{
+  double sum = 0.0;
+  for (unsigned long int i = 0; i < predicted_value.size(); i++)
+  {
+    double diff = predicted_value[i] - actual_value[i];
+    sum += (diff * diff);
+  }
+
+  return sum / predicted_value.size();
+}
+
+/**
+ * @brief run a single (x,y) pair with backpropagation algorithm.
+ *
+ */
+double MultiLayerPerceptron::bp(vector<double> x, vector<double> y)
+{
+  // Backpropagation step by step
+
+  // Step 1 : Feed a sample input to the network
+  vector<double> outputs = run(x);
+
+  // Step 2 : Calculate the MSE
+  // double mse = mean_squared_error(outputs, y);
+  vector<double> error;
+  double mse = 0.0;
+  for (unsigned long int i = 0; i < y.size(); i++)
+  {
+    error.push_back(y[i] - outputs[i]);
+    mse += error[i] * error[i];
+  }
+
+  mse /= layers.back();
+
+  // Step 3 : Calculate the o/p error terms
+  for (unsigned long int i = 0; i < outputs.size(); i++)
+  {
+    d.back()[i] = outputs[i] * (1 - outputs[i]) * error[i];
+  }
+
+  // Step 4 : Calculate the error term of each unit on each layer
+  for (unsigned long int i = network.size() - 2; i > 0; i--)
+  {
+    for (unsigned long int h = 0; h < network[i].size(); h++)
+    {
+      double fwd_error = 0.0;
+
+      for (int k = 0; k < layers[i + 1]; k++)
+      {
+        fwd_error += network[i + 1][k].weights[h] * d[i + 1][k];
+      }
+      d[i][h] = values[i][h] * (1 - values[i][h]) * fwd_error;
+    }
+  }
+
+  // Step 5 & 6 : Calculate the deltas and update the weights
+  for (unsigned long int i = 1; i < network.size(); i++)
+  {
+    for (int j = 0; j < layers[i]; j++)
+    {
+      for (int k = 0; k < layers[i - 1] + 1; k++)
+      {
+        double delta;
+        if (k == layers[i - 1])
+        {
+          delta = eta * d[i][j] * bias;
+        }
+        else
+        {
+          delta = eta * d[i][j] * values[i - 1][k];
+        }
+        network[i][j].weights[k] += delta;
+      }
+    }
+  }
+
+  return mse;
 }
