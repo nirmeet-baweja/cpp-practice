25876977c28957b4c285306e26d013104d87cbcb
nirmeet-baweja <nirmeet.baweja@gmail.com>
2022-11-17 19:14:28 +0000

    memoization of count_construct fn

--- algorithms/count_construct.cpp
+++ algorithms/count_construct.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<unordered_map>
 
 using namespace std;
 
-int count_construct(string target_string, vector<string> dict)
+int count_construct_memo(string target_string, vector<string> dict,
+                        unordered_map<string, int>& memo)
 {
+  if(memo.find(target_string) != memo.end())
+  {
+    return memo[target_string];
+  }
   if(target_string == "")
   {
     return 1;
@@ -16,14 +22,30 @@ int count_construct(string target_string, vector<string> dict)
     if(target_string.find(dict[i]) == 0)
     {
       string remaining_string = target_string.substr(dict[i].size());
-      count += count_construct(remaining_string, dict);
+      count += count_construct_memo(remaining_string, dict, memo);
     }
   }
-  return count;
+
+  memo[target_string] = count;
+  return memo[target_string];
+}
+
+int count_construct(string target_string, vector<string> dict)
+{
+  unordered_map<string, int> memo;
+  return count_construct_memo(target_string, dict, memo);
 }
 
 int main()
 {
-  cout << count_construct("abcdef", {"ab", "abc", "cd", "def", "abcd", "ef"}) << endl;
-  cout << count_construct("skateboard", {"bo", "rd", "ate", "t", "ska", "sk", "boar"}) << endl;
+  cout << count_construct("abcdef", {"ab", "abc", "cd", "def", "abcd", "ef"})
+       << endl;
+
+  cout << count_construct("skateboard",
+                        {"bo", "rd", "ate", "t", "ska", "sk", "boar"})
+        << endl;
+
+  cout << count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+                        {"e", "ee", "eee", "eeee", "eeeeee", "eeeeeeeee", "eeeeeeeeee"})
+       << endl;
 }
